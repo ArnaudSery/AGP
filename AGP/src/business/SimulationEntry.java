8f091c30f6e4ad772c3062848fd310952173b7e5
ArnaudSery <arnau@10.77.111.238>
2019-01-17 15:12:56 +0200

    commit

--- AGP/src/business/SimulationEntry.java
+++ AGP/src/business/SimulationEntry.java
@@ -17,7 +17,7 @@ public class SimulationEntry {
 			this.typeVisit = "";
 			this.keyWord = "";
 			
-			this.numberDay = 3;
+			this.numberDay = 5;
 			this.intensity = "hight";
 			if(intensity.compareTo("hight") == 0) {
 				this.numberDayExcursion = numberDay;
