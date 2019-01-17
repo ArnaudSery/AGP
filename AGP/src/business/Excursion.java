747afa7c2d90dfd798e0f40f8bc80054d2d9b3b0
ArnaudSery <arnau@10.77.111.238>
2019-01-17 11:49:08 +0200

    Function who create a LinkedList<Visit> and a RoundTrip is ok

--- AGP/src/business/Excursion.java
+++ AGP/src/business/Excursion.java
@@ -6,29 +6,55 @@ import java.util.LinkedList;
 public class Excursion {
 
 	private int price;
-	private LinkedList<Visit> visit;
+	private LinkedList<Visit> visits;
 	private RoundTrip roundTrip;
 	
 	public Excursion(LinkedList<Place> placeResult, Hotel hotel) {
 		
 		
-		this.visit = CreateVisit(placeResult);
-		this.roundTrip = CreateRoundTrip(visit, hotel);
+		this.visits = CreateVisit(placeResult, hotel);
+		this.roundTrip = CreateRoundTrip(visits, hotel);
 		
-		this.price = CalculPriceExcursion(this.visit, this.roundTrip);
+		this.price = CalculPriceExcursion(visits, roundTrip);
 		
 	}
 	
 	
 	private int CalculPriceExcursion(LinkedList<Visit> visits, RoundTrip roundTrip) {
 		
+		
 		return 0;
 	}
 
 
-	public static LinkedList<Visit> CreateVisit(LinkedList<Place> placeResult) {
+	public static LinkedList<Visit> CreateVisit(LinkedList<Place> placeResult, Hotel hotel) {
 		
 		LinkedList<Visit> visits = new LinkedList<Visit>();
+		
+		int time = Constante.TIME_EXCURSION;
+		
+			
+		for(int i = 0; i < placeResult.size(); i++) {
+			
+			time -= placeResult.get(i).getVisitDuration();
+			
+			if(i == 0) {
+				Visit visit = new Visit(hotel, placeResult.get(i));
+				visits.add(visit);
+			}
+			else {
+				if(time <= 0) {
+					break;
+				}
+				else {
+					Visit visit = new Visit(placeResult.get(i-1), placeResult.get(i));
+					visits.add(visit);
+				}
+			}
+			
+			
+		}
+		
 		
 		return visits;
 		
@@ -36,7 +62,7 @@ public class Excursion {
 	
 	public static RoundTrip CreateRoundTrip(LinkedList<Visit> visits, Hotel hotel) {
 		
-		RoundTrip roundTrip = new RoundTrip();
+		RoundTrip roundTrip = new RoundTrip(visits.getLast().getPlace(), hotel);
 		
 		return roundTrip;
 		
