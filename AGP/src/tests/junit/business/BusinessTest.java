8782d1198f0738be99e032b623d883b9a15f3f30
Entropy <Entropy@10.77.94.63>
2019-01-18 13:32:27 +0100

    Update JUnit test

--- AGP/src/tests/junit/business/BusinessTest.java
+++ AGP/src/tests/junit/business/BusinessTest.java
@@ -1,56 +1,197 @@
 package tests.junit.business;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.LinkedList;
+import java.util.List;
 
 import business.*;
 
 
 class BusinessTest {
-	
-	/*@Test
-	void testdistance() {
-		Coordinates coord1 = new Coordinates(14.58, 61.30);
-		Coordinates coord2 = new Coordinates(14.51, 61.12);
-		Coordinates coord3 = new Coordinates(-14.28, 60.58);
-		Coordinates coord4 = new Coordinates(14.25,-60.50);
-		Utility ut = new Utility();
-		double distance = ut.CalculDistance(coord1,coord2);
-		assertTrue(distance < 0.20 && distance > 0.19);
-		double distance2 = ut.CalculDistance(coord3,coord4);
-		assertTrue(distance2 < 125 && distance2 > 124);
+
+	/* =========== TEST COORDINATES =========== */ 
+	@Test
+	@DisplayName("Should be approximately close")
+	void testGetDistanceFrom() {
+		Coordinates departure = new Coordinates(15.31, -61.35);
+		Coordinates arrival = new Coordinates(14.49, -60.99);
+		
+		double actualResult = departure.getDistanceFrom(arrival);
+		double expectedMinResult = 30; // closest distance between Dominique and Martinique (search on GoogleMap)
+		double expectedMaxResult = 200; // furthest distance between Dominique and Martinique (search on GoogleMap)
+		arrival.setLatitude(20);
+		arrival.setLongitude(-50);
+		assertTrue(actualResult >= expectedMinResult && actualResult <= expectedMaxResult);
+	}
+
+	@Test
+	void testIsland() {
+		Island island = new Island(0);
+		Island island2 = new Island(1, "dom");
+
+		boolean isEquals = island.equals(island2);
+		island.incrementScore();
+		int id = island.getId();
+		String name = island.getName();
+		int score = island.getScore();
+
+		island.setId(2);
+		island.setName("guadeloupe");
+		island.setScore(100);
+	}
+
+	@Test
+	void testHotel() {
+		Coordinates hotelCoord = new Coordinates(15.31, -61.35);
+		Coordinates arrival = new Coordinates(14.49, -60.99);
+		Island isle = new Island(0);
+		Hotel hotel = new Hotel(1, "Bateliere", 50, hotelCoord, isle);
+		
+		hotel.incrementScore();
+		int id = hotel.getId();
+		String name = hotel.getName();
+		double price = hotel.getPricePerDay();
+		Coordinates coord = hotel.getCoordinates();
+		Island isle2 = hotel.getIsland();
+		int score = hotel.getScore();
+		
+		hotel.setCoordinates(arrival);
+		hotel.setId(3);
+		hotel.setIsland(isle);
+		hotel.setName("marti");
+		hotel.setPricePerDay(60);
+		hotel.setScore(600);
 	}
 	
 	@Test
+	void testPlace() {
+		Coordinates coord = new Coordinates(14.49, -60.99);
+		Coordinates coord2 = new Coordinates(17.49, -40.99);
+		Island isle = new Island(0);
+		Place place = new Place(0, "musée", true, 3, 20, "Cool", coord, isle, 50);
+		
+		int id = place.getId();
+		String name = place.getName();
+		boolean isH = place.isHistoric();
+		String desc = place.getDescription();
+		Coordinates coord3 = place.getCoordinates();
+		Island isle2 = place.getIsland();
+		int score = place.getScore();
+		
+		place.setCoordinates(coord2);
+		place.setId(3);
+		place.setIsland(isle);
+		place.setName("marti");
+		place.setScore(600);
+		place.setHistoric(false);
+		place.setVisitDuration(4);
+		place.setDescription("Trop cool");
+		place.setEntrancePrice(20);
+	}   
+	@Test
+	@DisplayName("Transport should be equals")
+	void testTransport() {
+		Transport bus = new Bus(70);
+		Transport boat = new Boat(200);
+
+		double actualBusResult = bus.getPriceForThisTravel();
+		double expectedBusResult = 17.5; // 70 * 0.25
+
+		double actualBoatResult = boat.getPriceForThisTravel();
+		double expectedBoatResult = 150; // 200 * 0.75
+		
+		double distance = boat.getDistanceTraveled();
+		
+		assertTrue(actualBusResult == expectedBusResult);
+		assertTrue(actualBoatResult == expectedBoatResult);
+	}
+
+	@Test
+	@DisplayName("Visit should be equals")
+	void testVisit() {
+		Transport bus = new Bus(70);
+		Place museum = new Place(0); // free museum on sundays
+		Visit visit = new Visit(bus, museum);
+
+		double actualPriceResult = visit.getPrice();
+		double expectedPriceResult = 17.5;
+
+		double actualDurationResult = visit.getDuration();
+		double expectedMinDurationResult = 0;
+		double expectedMaxDurationResult = 1;
+		
+		Transport newBus = visit.getTransportDriveway();
+		Place newPlace = visit.getPlaceToVisit();
+		
+		visit.setPlaceToVisit(new Place(0));
+		visit.setTransportDriveway(bus);
+
+		assertTrue(actualPriceResult == expectedPriceResult);
+		assertTrue(actualDurationResult >= expectedMinDurationResult && actualDurationResult <= expectedMaxDurationResult);
+	}
+
+	@Test
+	@DisplayName("Excursion should be smooth")
 	void testExcursion() {
-		SimulationEntry SimulationEntry = new SimulationEntry();
-	    LinkedList<Place> placeResult = new LinkedList<Place>();
-		LinkedList<Hotel> placeResultHotel = new LinkedList<Hotel>();
+		Transport boat = new Boat(200);
+		Place beach = new Place(0); // beach is free to go. Or is it?
+		Visit visit = new Visit(boat, beach);
+		Excursion excursion = new Excursion();
+
+		excursion.addVisit(visit);
+
+		int actualListResult = excursion.getPlacesToVisit().size();
+		int expectedListResult = 1;
+
+		Transport transport = excursion.getTransportReturnToHotel();
+		excursion.setTransportReturnToHotel(boat);
+		Boolean hasVisits = excursion.hasVisits();
+		double price = excursion.getPrice();
+		double duration = excursion.getDuration();
+
+		assertEquals(actualListResult, expectedListResult);
+	}
+
+	@Test
+	@DisplayName("Offer with a lot of excursion")
+	void testOffer() {
+		// For excursion
+		Transport boat = new Boat(200);
+		Place beach = new Place(0); // beach is free to go. Or is it?
+		Visit visit = new Visit(boat, beach);
+
+		Transport bus = new Bus(20);
+		Place foret = new Place(0); // beach is free to go. Or is it?
+		Visit visit2 = new Visit(bus, foret);
 		
-		Coordinates coord1 = new Coordinates(14.58, 61.30);
-		Coordinates coord2 = new Coordinates(14.51, 61.12);
-		Coordinates coord3 = new Coordinates(14.28, 60.58);
-		Coordinates coord4 = new Coordinates(14.25,60.50);
-		Coordinates coord5 = new Coordinates(14.23,60.52);
-		Coordinates coord6 = new Coordinates(14.26,60.50);
-		Island isle = new Island(0, "Guadeloupe");
-		Hotel HotelDuSoldatInconnu = new Hotel(0, "HotelDuSoldatInconnu", 200, coord1, isle);
-		Place restaurantDelArte = new HistoricPlace(1, "DelArte", 2, 100, "husihvgriuh", coord2, isle);
-		Place TourEiffel = new HistoricPlace(2, "TourEiffel", 6, 100, "husihvgriuh", coord3, isle);
-		Hotel hotel2 = new Hotel(3, "hotel2", 150, coord4, isle);
-		Place place1 = new ActivityPlace(4, "place1", 5, 150, "description", coord5, isle);
-		Place place2 = new ActivityPlace(5, "place2", 2, 150, "description",coord6, isle);
+		Excursion excursion = new Excursion();
+
+		excursion.addVisit(visit);
+		excursion.addVisit(visit2);
 		
-		// 
-		placeResult.add(restaurantDelArte);
-		placeResult.add(TourEiffel);
-		placeResult.add(place1);
-		placeResult.add(place2);
+		Hotel hotel = new Hotel(0);
+		Offer offer = new Offer(hotel, 4, 20);
+
+		offer.addExcursion(excursion);
+		boolean bool = offer.hasExcursionOnDay(4);
+		Excursion excursion2 = offer.getExcursionOfDay(4);
+		Hotel hotel2 = offer.getHotel();
+		List<Excursion> excursions = offer.getExcursions();
+		int freq = offer.getExcursionsFrequency();
+		int days = offer.getNumberOfDays();
+
+		Hotel bateliere = new Hotel(1);
+		offer.setHotel(bateliere);
+
+		offer.setExcursionsFrequency(2);
+		offer.setNumberOfDays(15);
+		offer.setExcursions(excursions);
 		
-		placeResultHotel.add(HotelDuSoldatInconnu);
-		placeResultHotel.add(hotel2);
-		
-		//Excursion.CreateExcursion(placeResult, placeResultHotel, SimulationEntry.getNumberHourMax());	
-	}*/
+		double price = offer.getPrice();
+
+
+	}
 }
